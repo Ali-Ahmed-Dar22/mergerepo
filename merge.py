2c0f1eda46fc217527204e1848a5d003532f2696
ALI-AHMED-DAR22 <alidar.92logics@gmail.com>
2025-01-01 22:23:56 +0500

    add father info

--- merge.py
+++ merge.py
@@ -1,6 +1,6 @@
 
 # THIS IS TO MEREGE REPO
 
-name = input("enter your name: ")
-age = input("enter your age: ")
-date = input("enter date of birth: ")
+name = input("enter your father name: ")
+age = input("enter your father age: ")
+date = input("enter your father date of birth: ")
